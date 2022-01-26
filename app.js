4aa14a5243c43bc405626ac0ee36a391257fb89b
Ghabryel <86971182+GhabryelHenrique@users.noreply.github.com>
2022-01-26 15:43:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const getPokemonUrl = id =>`https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromise = () => Array(150).fill().map((_, index) => 
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generateHTML = pokemons => {
+
+    return pokemons.reduce((accumulator, pokemon) => { 
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        
+        accumulator += `
+        <li class="card" ${types[0]}>
+        <img class="card-image" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png" />
+            <h2 class="card-title">${pokemon.id} ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+        </li>`
+        return accumulator
+    }, '')
+}
+
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    
+    ul.innerHTML = pokemons
+}
+
+const fathPokemon = () => {
+    const pokemonPromises = generatePokemonPromise()
+
+        Promise.all(pokemonPromises)
+        .then(generateHTML)
+        .then(insertPokemonsIntoPage)
+
+}
+
+fathPokemon()
